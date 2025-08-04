801edd06d28584ccc90095fea40d3406c970288b
W1nd0w55 <iliader@outlook.com>
2025-08-04 14:19:38 +0500

    added backend api server setting

--- frontend/web/views.py
+++ frontend/web/views.py
@@ -1,7 +1,13 @@
-from django.views import View
+#from django.views import View
 from django.views.generic import TemplateView
-from django.http import HttpResponse
+from django.conf import settings
 
 # Create your views here.
 class RootView(TemplateView):
     template_name = 'root.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['backend_api_server'] = settings.BACKEND_API_SERVER
+
+        return context
