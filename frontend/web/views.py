dc5ec4710f8dbd2ea68f1c730d3583bb92b29b19
W1nd0w55 <iliader@outlook.com>
2025-08-04 12:55:18 +0500

    added root view

--- frontend/web/views.py
+++ frontend/web/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
+class RootView(View):
+    def get(self, request):
+        return HttpResponse("Hi!")
