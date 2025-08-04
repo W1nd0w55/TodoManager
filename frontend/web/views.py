0aaa6d4b0601201d6a0b78edabd0c51eb2e01cd1
W1nd0w55 <iliader@outlook.com>
2025-08-04 13:06:56 +0500

    added root template

--- frontend/web/views.py
+++ frontend/web/views.py
@@ -3,6 +3,5 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 # Create your views here.
-class RootView(View):
-    def get(self, request):
-        return HttpResponse("Hi!")
+class RootView(TemplateView):
+    template_name = 'root.html'
