d14359ddccbc8941c10b30e89b942a6f576d7b50
W1nd0w55 <iliader@outlook.com>
2025-10-23 22:54:54 +0500

    added links which i somehow forgot to add

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -2,9 +2,14 @@ import Card from '../components/Card';
 import '../css/Main.css';
 
 export default () => {
+	//TODO: add tailwind
 	return (<Card>
-		<h2 style={{marginTop: 0}}>Plan your day like never before.</h2>
-		<p style={{marginBottom: 0}}>With TodoManager, you can manage tasks,<br />
+		<h2 style={{ marginTop: 0 }}>Plan your day like never before.</h2>
+		<p>With TodoManager, you can manage tasks,<br />
 		and track the amount of work you've done.</p>
+		<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '20px' }}>
+			<a className="btn-like-link" href="/login">Log In</a>
+			<a className="btn-like-link" href="/signup">Sign Up</a>
+		</div>
 	</Card>);
 };
