5d19ed50a2c113b917f4ceee34cde25b5e69ea9d
W1nd0w55 <iliader@outlook.com>
2025-10-24 17:45:16 +0500

    applied

--- frontend/pages/Index.tsx
+++ frontend/pages/Index.tsx
@@ -3,10 +3,10 @@ import ButtonLikeLink from '../components/ButtonLikeLink';
 
 export default () => {
 	return <Card>
-		<h2 style={{ marginTop: 0 }}>Plan your day like never before.</h2>
-		<p>With TodoManager, you can manage tasks,<br />
+		<h2 className="mb-5 font-bold text-[30pt]">Plan your day like never before.</h2>
+		<p className="mb-5">With TodoManager, you can manage tasks,<br />
 		and track the amount of work you've done.</p>
-		<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '20px' }}>
+		<div className="flex flex-row justify-center gap-5">
 			<ButtonLikeLink href="/login">Log In</ButtonLikeLink>
 			<ButtonLikeLink href="/signup">Sign Up</ButtonLikeLink>
 		</div>
