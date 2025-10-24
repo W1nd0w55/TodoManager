e9fc10c94fd29f64d2a6bad5bbedbd85657f13b5
W1nd0w55 <iliader@outlook.com>
2025-10-24 17:40:14 +0500

    added card tailwind style

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -1,12 +1,11 @@
 import type { ReactNode } from 'react';
-import '../css/Card.css';
 
 interface Props {
     children: ReactNode;
 };
 
 export default (props: Props) => {
-    return <div className="card">
+    return <div className="bg-[#32fff5] rounded-[10px] p-5 text-center" style={{ boxShadow: "0px 0px 10px black" }}>
         {props.children}
     </div>;
 };
