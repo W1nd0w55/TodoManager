b97d76b55579734321a585b7c730cfd919f44953
W1nd0w55 <iliader@outlook.com>
2025-10-24 17:34:01 +0500

    added buttonlikelink tailwind style

--- frontend/components/ButtonLikeLink.tsx
+++ frontend/components/ButtonLikeLink.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from 'react';
 
 interface Props {
     href: string;
-    children: ReactNode
+    children: ReactNode;
 };
 
 export default (props: Props) => {
-    return <a className="btn-like-link" href={props.href}>{props.children}</a>;
+    return <a className="text-white no-underline bg-[#f0ad67] p-2.5 rounded-[5px]" href={props.href}>{props.children}</a>;
 };
